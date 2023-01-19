8e4f00d191a23726d591021debb4162cbf4572c7
eduardaperez <97349220+eduardaperez@users.noreply.github.com>
2023-01-18 21:07:46 -0300

    mais controle de fluxo

---
+++ src/main/kotlin/07.4_controleDeFluxoDoWhile.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    var i = 0
+    // do, primeiro executa e depois verifica
+    do {
+        print("$i ")
+        i++
+    }while (i < 10)
+
+    println("")
+
+    do {
+        print("Qual o seu nome?: ")
+        val value = readLine()
+    } while (value == "")
+}
